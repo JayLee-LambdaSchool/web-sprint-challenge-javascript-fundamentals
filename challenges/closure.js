e7ce92e5907aa35a2bc438cfabb021ddb09ab887
dreampoetlee <dreampoetlee@gmail.com>
2020-06-14 22:57:13 -0400

    Completed working on Sprint Challenge - Completed the closure challenge

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,25 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// Closure is a way to understand how different functions can see outside of themselves and into the global scope. Which is why everything inside of the curly braces is enclosed and known as the closure. It's like a one way mirror.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  let count = 0;
+
+  for (let i = 0; i < num; i++) {
+    // console.log(i + 1)
+    function addIndex(num) {
+      return num + 1
+    }
+    // console.log(addIndex(i))
+    count = count + addIndex(i)
+  }
+
+  return count
+}
+
+console.log(summation(4));
